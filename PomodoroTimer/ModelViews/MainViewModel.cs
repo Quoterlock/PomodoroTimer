1c000c96cf0480b43d023e2f4c25dbfdd979f364
Vladislav Kyrpa <vladkrp2003@gmail.com>
2023-05-18 13:31:38 +0300

    To C# v10.0

---
+++ PomodoroTimer/ModelViews/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using PomodoroTimer.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PomodoroTimer.ViewModels
+{
+    internal class MainViewModel : ViewModelBase
+    {
+        public ViewModelBase CurrentViewModel => Navigator.CurrentViewModel;
+        public MainViewModel()
+        {
+            Navigator.CurrentViewModelChanged += OnCurrentViewModelChanged;
+        }
+
+        private void OnCurrentViewModelChanged()
+        {
+            OnPropertyChanged(nameof(CurrentViewModel));
+        }
+    }
+}
---
+++ PomodoroTimer/Services/Navigator.cs
@@ -0,0 +1,30 @@
+﻿using PomodoroTimer.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PomodoroTimer.Services
+{
+    public class Navigator
+    {
+        private ViewModelBase? currentViewModel;
+
+        // when the CurrentViewModelChanged has changed event
+        public event Action? CurrentViewModelChanged;
+
+        public ViewModelBase? CurrentViewModel
+        {
+            get => currentViewModel;
+            set
+            {
+                if(currentViewModel != value)
+                {
+                    currentViewModel = value;
+                    OnCurrentViewModelChanged();
+                }
+            }
+        }
+    }
+}
